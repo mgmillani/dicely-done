58c19e2bacf159356ad38eb2c5e9d521ef3dbf40
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2015-05-24 17:48:46 -0300

    Updated for the new face detection algorithm

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "icedice.h"
-#include "circles.h"
-#include "squares.h"
+#include "icedice.hpp"
+#include "circles.hpp"
+#include "squares.hpp"
+#include "faces.hpp"
 
 #include "debug.h"
 
@@ -21,9 +22,9 @@ int cP1 = 100;
 int cP2 = 15;
 int cMaxMeanSqr = 15;
 int sMaxMeanSqr = 15;
+int sMinLength = 10;
 
-char circWindow[] = "Circle";
-char sqrWindow[] = "Square";
+char window[] = "Faces";
 
 typedef struct
 {
@@ -54,54 +55,27 @@ int main(int argc, char** argv)
   if( !src.data )
     { return -1; }
 
-  /// Convert it to gray
-  cvtColor( src, src_gray, CV_BGR2GRAY );
-  
-	vector<Vec3f> circles = findCircles(src_gray, &circ_img, cMaxMeanSqr);
-	std::vector<cv::Vec4i> lines = findLines(src_gray, &line_img);
-	
-	//cvtColor(circ_img, circ_img, CV_GRAY2BGR);
-	cvtColor(line_img, line_img, CV_GRAY2BGR);
-	
-	//src = dst;
-	
-
-	/// Draw the circles detected	
-  for( size_t i = 0; i < circles.size(); i++ )
+  std::vector<t_face> faces = findFaces(&src);
+  for(size_t i=0 ; i<faces.size() ; i++)
   {
-		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
-		// circle center
-		circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
-		circle( circ_img, center, 3, Scalar(0,255,0), -1, 8, 0 );
-		// circle outline
-		circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
-		circle( circ_img, center, radius, Scalar(0,0,255), 1, 8, 0 );
-	}
-	
-	for( size_t i=0 ; i<lines.size() ; i++)
-	{
-		cv::Vec4i v = lines[i];
-		cv::line(src, cv::Point(v[0], v[1]), cv::Point(v[2], v[3]), CV_RGB(0,0,255));
-		cv::line(line_img, cv::Point(v[0], v[1]), cv::Point(v[2], v[3]), CV_RGB(0,0,255));
-	}
+		t_face face = faces[i];
+		Point center(face.center[0], face.center[1]);
+		int v = face.value;
+		TRACE("Value:%d\n",v);
+		circle(src, center, 20, Scalar(255 * (v&1) ,255 * (v&2), 255 * (v&4)), 1, 8, 0);
+  }
 
   /// Show your results
-  namedWindow( circWindow, CV_WINDOW_AUTOSIZE );
-  //imshow( "Hough Circle Transform Image", src );
-  imshow( circWindow, circ_img );
-  namedWindow( sqrWindow, CV_WINDOW_AUTOSIZE );
-  imshow( sqrWindow, line_img );
-  //imshow( "Hough Circle Transform Lines", line_img );
+  namedWindow( window, CV_WINDOW_AUTOSIZE );
+  imshow( window, src );
 
-
-	infoC.src = infoS.src =  src;
-	infoC.src_gray = infoC.src_gray = src_gray;
+	/*infoC.src = infoS.src =  src;
+	infoC.src_gray = infoS.src_gray = src_gray;
 
 	char* trackbar_label = "Treshold 1:";
   createTrackbar( trackbar_label, circWindow, &cMaxMeanSqr, 500, CircleMethod, &infoC );
   createTrackbar( trackbar_label, sqrWindow, &sMaxMeanSqr, 500, SquareMethod, &infoS );
-
+	*/
   while((char)waitKey(0) != 'q')
 		;
   return 0;
@@ -110,7 +84,7 @@ int main(int argc, char** argv)
 /** @function CircleMethod */
 void CircleMethod( int t, void* infoP )
 {
-	t_info *info = (t_info *)infoP;
+	/*t_info *info = (t_info *)infoP;
 	if ( t==0)
 		t = 1;
 	
@@ -130,14 +104,17 @@ void CircleMethod( int t, void* infoP )
 	}
 	
 	namedWindow( circWindow, CV_WINDOW_AUTOSIZE );
-	imshow( circWindow, info->view );
+	imshow( circWindow, info->view );*/
 	
 }
 
 void SquareMethod(int t, void* infoP)
 {
-	t_info *info = (t_info *)infoP;
-	std::vector<cv::Vec4i> lines = findLines(info->src_gray, &info->view);
+	/*t_info *info = (t_info *)infoP;
+	
+	std::vector<cv::Vec4i> lines = findLines(info->src_gray, t, &info->view);
+	
+	cvtColor( info->view, info->view, CV_GRAY2BGR );
 	
 	for( size_t i=0 ; i<lines.size() ; i++)
 	{
@@ -146,6 +123,6 @@ void SquareMethod(int t, void* infoP)
 		cv::line(info->view, cv::Point(v[0], v[1]), cv::Point(v[2], v[3]), CV_RGB(0,0,255));
 	}
 	
-	namedWindow( circWindow, CV_WINDOW_AUTOSIZE );
-	imshow( circWindow, info->view );
+	namedWindow( sqrWindow, CV_WINDOW_AUTOSIZE );
+	imshow( sqrWindow, info->view );*/
 }
