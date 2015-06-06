2907b67272bfc595eeb21ab3a816966aa3ed387f
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2015-06-05 21:54:52 -0300

    List with active players; Minimum bet per round; Waits for ack after a hand is received; Receives folds; I should commit more often; Next player and round on their own methods; Notification messages; I should definitely commit more often.

--- server/src/game.cpp
+++ server/src/game.cpp
@@ -73,7 +73,7 @@ Game::Rank getRank(const Player *p)
 	if(straight)
 		return Game::Rank::STRAIGHT;
 	// full house
-	unsigned int v = h.values[0];
+	int v = h.values[0];
 	int row = 1;
 	bool pair = false;
 	bool toak = false;
@@ -103,68 +103,85 @@ Game::Rank getRank(const Player *p)
 
 Game::Game()
 {
-	this->currentPlayer = this->players.begin();
 	this->round = Game::Round::INITIAL;
-	this->needed = Game::Info::HAND;
+	this->needed = Game::Info::NOTHING;
+	this->pot = 0;
+	this->minBet = 5;
+	this->playerBet = minBet;
 	this->startScore = 45;
 }
 	
 void Game::join(string player)
 {
 	Player *p = new Player(player, this->startScore);
-	
 	this->players.push_back(p);
+	if(this->round == Round::INITIAL)
+		this->activePlayers.push_back(p);
 	if(this->players.size() == 1)
-		this->currentPlayer = this->players.begin();
+	{
+		this->currentPlayer = this->activePlayers.begin();
+		this->needed = Game::Info::HAND;
+		this->informPlayer();
+	}
+	this->pot += this->minBet;
+	p->bet = this->minBet;
+	this->informJoin(p);
 }
+
 void Game::quit(string player)
 {
-	for(list<Player*>::iterator it ; it!=players.end() ; it++)
+	for(list<Player*>::iterator it = this->activePlayers.begin() ; it!=activePlayers.end() ; it++)
 	{
 		Player *p = *it;
 		if(p->name.compare(player) == 0)
 		{
-			players.erase(it);
+			this->activePlayers.erase(it);
+			break;
+		}
+	}
+	for(list<Player*>::iterator it = this->players.begin() ; it!=players.end() ; it++)
+	{
+		Player *p = *it;
+		if(p->name.compare(player) == 0)
+		{
+			this->informQuit(p);
+			players.erase(it);			
 			delete p;
 			return;
 		}		
 	}
 }
+
 void Game::giveHand(t_hand hand)
 {
 	Player *p = *this->currentPlayer;
-	ERR("Now playing: %s\n", p->name.c_str());
-	Round next;
 	switch(this->round)
 	{
 		case Round::INITIAL:
-			p->hand = hand;
-			this->currentPlayer++;
-			next = Round::BET;
+			if(hand.len == 5)
+			{
+				p->hand = hand;
+				this->informHand(p);
+				this->needed = Game::Info::HAND_ACK;
+			}
 			break;
 		case Round::RECAST:
-			p->hand = hand;
-			this->currentPlayer++;
-			next = Round::RESULT;
+			if(hand.len == 5)
+			{
+				p->hand = hand;
+				this->informHand(p);
+				this->needed = Game::Info::HAND_ACK;
+			}
 			break;
 		case Round::RESULT:
 		case Round::MATCH:
 		case Round::BET:
 			break;
 	}
-		
-	if(this->currentPlayer == this->players.end())
-	{
-		this->round = next;
-		this->updateNeeded();
-		this->currentPlayer = this->players.begin();
-	}
 }
 void Game::giveBet(t_score bet)
 {
 	Player *p = *this->currentPlayer;
-	ERR("Now playing: %s\n", p->name.c_str());
-	Round next = this->round;;
 	switch(this->round)
 	{
 		case Round::INITIAL:
@@ -174,51 +191,81 @@ void Game::giveBet(t_score bet)
 		case Round::RESULT:
 			break;
 		case Round::MATCH:
-			p->score -= bet;
-			p->bet += bet;
-			next = Round::RECAST;
-			this->currentPlayer++;
-			break;
+			if(p->bet + bet > this->playerBet)
+				break;
 		case Round::BET:
-			p->score -= bet;
-			p->bet += bet;
-			next = Round::MATCH;
-			this->currentPlayer++;
+			if(p->bet + bet >= this->playerBet)
+			{
+				p->score -= bet;
+				p->bet += bet;
+				this->playerBet = p->bet;
+				this->pot += bet;
+				this->informBet(p);
+				this->nextPlayer();
+			}
 			break;
-	}
-		
-	if(this->currentPlayer == this->players.end())
-	{
-		this->round = next;
-		this->updateNeeded();
-		this->currentPlayer = this->players.begin();
 	}
 }
+
+void Game::giveFold()
+{
+	Player *p = *this->currentPlayer;
+	switch(this->round)
+	{
+		case Round::INITIAL:
+		case Round::RECAST:
+		case Round::RESULT:
+			break;
+		case Round::MATCH:			
+		case Round::BET:
+			this->activePlayers.erase(this->currentPlayer);
+			this->informFold(p);			
+			this->nextPlayer();
+			break;
+	}
+}
+
+void Game::giveHandAck()
+{
+	switch(this->round)
+	{
+		case Round::INITIAL:
+		case Round::RECAST:
+			this->needed = Game::Info::HAND;
+			this->nextPlayer();
+			break;
+		default:
+			break;
+	}
+}
+
 void Game::giveAck()
 {
-	if(this->round == Round::RESULT)
+	switch(this->round)
 	{
-		//calculates the winner
-		this->decideWinner();
-		// winner gets the pot
-		Player *p = *this->winner;
-		ERR("%s won!\n", p->name.c_str());
-		p->score += this->pot;
-		// clears the pot
-		this->pot = 0;
-		// resets player bets
-		for(list<Player*>::iterator it = this->players.begin() ; it!=this->players.end() ; it++)
-		{
-			(*it)->bet = 0;
-		}
-		// rotate player order
-		this->players.push_back(*this->players.begin());
-		this->players.pop_front();
-		// start from first player
-		this->currentPlayer = this->players.begin();
-		// go back to the first round
-		this->round = Round::INITIAL;
-		this->updateNeeded();
+
+		case Round::RESULT:
+			// clears the pot
+			this->pot = 0;
+			// resets player bets
+			for(list<Player*>::iterator it = this->players.begin() ; it!=this->players.end() ; it++)
+			{
+				(*it)->bet = 0;
+			}
+			// rotate player order
+			this->players.push_back(*this->players.begin());
+			this->players.pop_front();
+			this->activePlayers = this->players;
+			// start from first player
+			this->currentPlayer = this->players.begin();
+			// go back to the first round
+			this->round = Round::INITIAL;
+			this->updateNeeded();
+			this->informPlayer();
+			this->informRound();
+			break;
+		default:
+			break;
 	}
 }
 	
@@ -229,16 +276,13 @@ void Game::updateNeeded()
 		case Round::INITIAL:
 		case Round::RECAST:
 			this->needed = Info::HAND;
-			ERR("Throw your dice\n");
 			break;
 		case Round::BET:
 		case Round::MATCH:
 			this->needed = Info::BET;
-			ERR("Place your bet\n");
 			break;
 		case Round::RESULT:
 			this->needed = Info::ACK;
-			ERR("Finish\n");
 			break;
 	}
 }
@@ -246,7 +290,7 @@ void Game::updateNeeded()
 void Game::decideWinner()
 {
 	int rank = (int)(Game::Rank::HIGHEST)+1;
-	for(list<Player*>::iterator it=this->players.begin() ; it!=this->players.end() ; it++)
+	for(list<Player*>::iterator it=this->activePlayers.begin() ; it!=this->activePlayers.end() ; it++)
 	{
 		Player *p = *it;
 		p->rank = getRank(p);
@@ -256,4 +300,100 @@ void Game::decideWinner()
 			this->winner = it;
 		}
 	}
+}
+
+void Game::nextPlayer()
+{
+	this->currentPlayer++;	
+	
+	if(this->currentPlayer == this->activePlayers.end())
+	{
+		this->nextRound();
+		this->updateNeeded();
+		this->currentPlayer = this->activePlayers.begin();
+	}
+	this->informPlayer();
+}
+
+void Game::nextRound()
+{
+	switch(this->round)
+	{
+		case Round::INITIAL: this->round = Round::BET; break;
+		case Round::RECAST:  this->round = Round::RESULT;
+		{
+			//calculates the winner
+			this->decideWinner();
+			// winner gets the pot
+			Player *p = *this->winner;
+			p->score += this->pot;
+			this->informWinner();
+		}
+			break;
+		case Round::BET:     this->round = Round::MATCH; break;
+		case Round::MATCH:   this->round = Round::RECAST; break;
+		case Round::RESULT:  this->round = Round::INITIAL; break;
+	}
+	this->updateNeeded();
+	this->informRound();
+}
+
+void Game::informStart()
+{
+	printf("startgame\n");
+}
+void Game::informPlayer()
+{
+	//printf("Playing: %s\n", (*this->currentPlayer)->name.c_str());
+}
+
+void Game::informRound()
+{
+	
+	switch(this->round)
+	{
+		case Round::INITIAL: 
+		case Round::RECAST:  
+			printf("startturn %d\n", (int)this->round);
+			break;
+		case Round::RESULT: break;
+		case Round::BET:     
+		case Round::MATCH:   printf("startturn %d %u\n", (int)this->round, this->playerBet); break;
+		
+	}
+}
+
+void Game::informWinner()
+{
+	printf("endgame %s %d\n", (*this->winner)->name.c_str(), this->pot);
+}
+
+void Game::informHand(Player *p)
+{
+	printf("dice %s ", p->name.c_str());
+	for(int i=0 ; i<p->hand.len ; i++)
+	{
+		printf("%d ", p->hand.values[i]);
+	}
+	printf("\n");
+}
+
+void Game::informBet(Player *p)
+{
+	printf("betplaced %s %d %d\n", p->name.c_str(), p->bet, this->pot);
+}
+
+void Game::informFold(Player *p)
+{
+	printf("folded %s\n", p->name.c_str());
+}
+
+void Game::informQuit(Player *p)
+{
+	printf("quit %s\n", p->name.c_str());
+}
+
+void Game::informJoin(Player *p)
+{
+	printf("join %s\n", p->name.c_str());
 }
