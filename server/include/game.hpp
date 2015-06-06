1b3ff9c307f4c738d420c1bb80ea44aae2230119
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2015-06-05 21:44:20 -0300

    Functions that inform someone of what is going on. Should be overriden to allow internet connection

--- server/include/game.hpp
+++ server/include/game.hpp
@@ -18,8 +18,8 @@ bool comparePlayerHand(const Player *first, const Player *second);
 class Game
 {
 public:
-	enum class Round {INITIAL, BET, MATCH, RECAST, RESULT};
-	enum class Info {HAND, BET, ACK};
+	enum class Round {INITIAL=1, BET, MATCH, RECAST, RESULT};
+	enum class Info {HAND, BET, ACK, HAND_ACK, NOTHING};
 	enum class Rank
 	{ NONE=0
 	, FIBONACCI
@@ -35,18 +35,71 @@ public:
 	void join(std::string player);
 	void quit(std::string player);
 	void giveHand(t_hand hand);
+	void giveHandAck();
 	void giveBet(t_score bet);	
+	void giveFold();
 	void updateNeeded();
 	void giveAck();
 	void decideWinner();
+	void nextPlayer();
+	void nextRound();
+	/**
+	 * notification functions. Used to signalize someone of what is going on
+	 */
+	/**
+	 * let the game begin!
+	 */
+	void informStart();
+	/**
+	 * who is the current player
+	 */
+	void informPlayer();
+	/**
+	 * what is the current round
+	 */
+	void informRound();
+	/**
+	 * which is the hand of the given player
+	 */
+	void informHand(Player *p);
+	/**
+	 * total bet of the given player
+	 */
+	void informBet(Player *p);
+	/**
+	 * given player gave up
+	 */
+	void informFold(Player *p);
+	/**
+	 * given player left the game
+	 */
+	void informQuit(Player *p);
+	/**
+	 * given player has joined
+	 */
+	void informJoin(Player *p);
+	/**
+	 * who won the game
+	 */
+	void informWinner();
 	
 	std::list<Player*> players;
+	std::list<Player*> activePlayers;
 	std::list<Player*>::iterator currentPlayer;
 	std::list<Player*>::iterator winner;
 	Game::Round round;
 	Game::Info needed; // what kind of information the game needs to be continue
 	t_score pot; // accumulated bets
+	t_score playerBet; // how much each player is betting
 	t_score startScore; // with how many points each player starts
+	t_score minBet; // how much each player bets when the game starts
+};
+
+class RemoteGame : public Game
+{
+	void informPlayer();
+	void informRound();
+	void informWinner();
 };
 
 /**
