25224bae6e4708ce58ad4bc2a500564ff694a6b6
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2015-06-27 16:50:58 -0300

    Counts frequency of each face for virtual and real dice

--- logparser.py
+++ logparser.py
@@ -5,6 +5,29 @@ import sys
 class Round():
 	pass
 
+def csvDice(real, virtual):
+	print("face,frequency,type")
+	f = 1
+	for r in real[1:]:
+		print(f, r, end=', ', sep=',')
+		print('real')
+		f+=1
+	f = 1
+	for r in virtual:
+		print(f, r, end=', ', sep=',')
+		print('virtual')
+		f+=1
+	
+def csvRound(players):
+	print("player, r1, r2, r3, r4")
+	for p in players:
+		print(p, end=', ')
+		for r in players[p][:-1]:
+			print(r.t, end=', ')
+		print(players[p][-1].t)
+		#print(r.t, r.player, r.n)
+
+
 argv = sys.argv
 
 if len(argv) == 1:
@@ -13,18 +36,23 @@ else:
 	gameDurations = []
 	rounds = []
 	players = {}
+	realDice = [0,0,0,0,0,0,0]
+	virtualDice = [0,0,0,0,0,0,0]
 	for fl in argv[1:]:
 		with open(fl) as f:
+			#print(fl)
 			lines = f.readlines()
 			tg = 0
 			tr = 0
 			player = ''
+			n = 1
 			for l in lines:
 				words = l.split(" ")
 				t = float(words[0][:-2])
 				#print(t)
 				if words[2] == 'startgame':
 					tg = t
+					virtual = False
 					player = words[1]
 				elif words[2] == 'endgame':
 					gameDurations.append(t - tg)
@@ -50,13 +78,15 @@ else:
 						
 					tr = t
 					n = int(words[3])
-	# print("Duration of each game:")				
-	# for g in gameDurations:
-	# 	print(g)
-	print("player, r1, r2, r3, r4")
-	for p in players:
-		print(p, end=', ')
-		for r in players[p][:-1]:
-			print(r.t, end=', ')
-		print(players[p][-1].t)
-		#print(r.t, r.player, r.n)
+				elif words[2] == 'dice':
+					for d in words[4:]:
+						if virtual:
+							virtualDice[int(d)] += 1
+						else:
+							realDice[int(d)] += 1
+				elif words[2] == 'roll':
+					virtual = True
+					
+	#csvRound(players)
+	csvDice(realDice, virtualDice)
+
