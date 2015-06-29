0d9f2433acb2104a68f81fe35875e1aa65b669f9
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2015-06-29 15:14:06 -0300

    R script that draws some graphs and runs some t-tests

---
+++ experiments/analysis.r
@@ -0,0 +1,72 @@
+#!/bin/Rscript
+
+library(ggplot2)
+
+########
+# DICE #
+########
+
+
+rd <- read.csv('data/real-dice-freq.csv')
+vd <- read.csv('data/virtual-dice-freq.csv')
+
+reals <- rep('real', length(rd$freq))
+virtuals <- rep('virtual', length(vd$freq))
+dice <- data.frame(face = c(rd$face, vd$face), freq=c(rd$freq, vd$freq), type = c(reals,  virtuals))
+
+dice
+
+pdf('dice-freq.pdf', width=4, height=3.25)
+
+rdAvg <- sum(rd$freq) / 6
+rdAvg
+vdAvg <- sum(vd$freq) / 6
+vdAvg
+
+ggplot(dice, aes(x=face, y=freq, fill=type))+#
+geom_bar(stat='identity', position='dodge')+#
+scale_x_continuous(breaks=c(1,2,3,4,5,6))+#
+xlab('Face')+ylab('Frequência')+#
+geom_hline(yintercept=rdAvg, linetype='dotted') +#
+geom_hline(yintercept=vdAvg, linetype='dashed') +#
+theme(legend.position='top'
+        , legend.title=element_blank()
+        , legend.background = element_rect(fill = "transparent",colour = NA)
+        , plot.background = element_rect(fill = "transparent",colour = NA))
+
+############
+"Velocidade"
+############
+
+vt <- read.csv('data/virtual-time.csv')
+rt <- read.csv('data/real-time.csv')
+
+t.test(rt$r1, vt$r1, alternative='greater')
+t.test(rt$r4, vt$r4, alternative='greater')
+
+timing <- data.frame(
+    player=unlist(list(rt$player, vt$player))
+  , r1=c(rt$r1, vt$r1)
+  , r4=c(rt$r4, vt$r4)
+  , type=c(rep('reais', length(rt$r1)) , rep('virtuais', length(vt$r1))))
+
+pdf('rl-vt-r1.pdf', width=4, height = 3.25, bg='transparent')
+ggplot(timing, aes(x=type, y=r1, colour=type)) +#
+geom_boxplot() +scale_x_discrete(breaks=NULL)+#
+#geom_jitter(position = position_jitter(width=0.3))+#
+theme(legend.position='top'
+        , legend.title=element_blank()
+        , legend.background = element_rect(fill = "transparent",colour = NA)
+        , plot.background = element_rect(fill = "transparent",colour = NA))+#
+xlab('')+ylab('Tempo[s]')
+
+pdf('rl-vt-r4.pdf', width=4, height = 3.25)
+ggplot(timing, aes(x=type, y=r4, colour=type)) +#
+geom_boxplot() +scale_x_discrete(breaks=NULL)+#
+#geom_jitter(position = position_jitter(width=0.3))+#
+    theme(legend.position='top'
+        , legend.title=element_blank()
+        , legend.background = element_rect(fill = "transparent",colour = NA)
+        , plot.background = element_rect(fill = "transparent",colour = NA))+#
+xlab('')+ylab('Tempo[s]')
+        
