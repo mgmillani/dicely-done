dddfc671c66863b337282a2daac0aab6720c8330
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2015-06-29 17:40:14 -0300

    Replaces unknown entries with ??

--- experiments/parse-answers.sh
+++ experiments/parse-answers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat data/answers.csv | sed 's/Com auxílio/Com/g' | sed 's/Sem auxílio/Sem/g' | sed 's/(muito melhor)/\+/g' | sed 's/Dados //g' > data/answers-parse.csv
+cat data/answers.csv | sed 's/Com auxílio/Com/g' | sed 's/Sem auxílio/Sem/g' | sed 's/(muito melhor)/\+/g' | sed 's/Dados //g' | sed 's/,","/,\?\?/g' > data/answers-parse.csv
