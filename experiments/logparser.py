eae55de3a5e8365f0482eb14873135393bf6db3a
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2015-06-29 15:13:17 -0300

    Fixed some issues with the logparser

--- experiments/logparser.py
+++ experiments/logparser.py
@@ -12,20 +12,18 @@ def csvDice(real, virtual):
 		print(f, r, end=', ', sep=',')
 		print('real')
 		f+=1
-	f = 1
-	for r in virtual:
-		print(f, r, end=', ', sep=',')
-		print('virtual')
-		f+=1
-	
-def csvRound(players):
+
+def csvRound(games):
 	print("player, r1, r2, r3, r4")
-	for p in players:
-		print(p, end=', ')
-		for r in players[p][:-1]:
-			print(r.t, end=', ')
-		print(players[p][-1].t)
-		#print(r.t, r.player, r.n)
+	for g in games:
+		players = g
+		for p in players:
+			print(p, end=', ')
+			pad = len(players[p]) - 4
+			for r in players[p][:-1]:
+				print(r.t, end=', ')
+			print(players[p][-1].t, end='')
+			print(','*pad)
 
 
 argv = sys.argv
@@ -33,11 +31,12 @@ argv = sys.argv
 if len(argv) == 1:
 	print("usage: %s <FILES...>"%(argv[0]))
 else:
-	gameDurations = []
 	rounds = []
 	players = {}
 	realDice = [0,0,0,0,0,0,0]
 	virtualDice = [0,0,0,0,0,0,0]
+	dice = []
+	games = []
 	for fl in argv[1:]:
 		with open(fl) as f:
 			#print(fl)
@@ -48,21 +47,26 @@ else:
 			n = 1
 			for l in lines:
 				words = l.split(" ")
+				#print(l)
 				t = float(words[0][:-2])
 				#print(t)
 				if words[2] == 'startgame':
 					tg = t
 					virtual = False
 					player = words[1]
+					players = {}
 				elif words[2] == 'endgame':
-					gameDurations.append(t - tg)
+					#gameDurations.append(t - tg)
 					r = Round()
 					r.t = t - tr
 					r.player = player
 					r.n = n
 					rounds.append(r)
-					#print(l, player, n)
 					players[player].append(r)
+					games.append(players)
+					#print(len(players[player]))
+					players = {}
+					player = ''
 				elif words[2] == 'startturn':
 					if player != words[1]:
 						r = Round()
@@ -72,21 +76,18 @@ else:
 						rounds.append(r)
 						if not player in players:
 							players[player] = []
-						#print(l, player, n)
 						players[player].append(r)
+						#print(player, len(players[player]))
 						player = words[1]
-						
-					tr = t
-					n = int(words[3])
-				elif words[2] == 'dice':
+						for d in dice:
+							realDice[d] += 1
+						dice = []
+						tr = t
+						n = int(words[3])
+				elif words[2] == 'dice' and n==1:
+					dice = []
 					for d in words[4:]:
-						if virtual:
-							virtualDice[int(d)] += 1
-						else:
-							realDice[int(d)] += 1
-				elif words[2] == 'roll':
-					virtual = True
-					
-	#csvRound(players)
-	csvDice(realDice, virtualDice)
+						dice.append(int(d))
 
+	csvRound(games)
+	#csvDice(realDice, virtualDice)
